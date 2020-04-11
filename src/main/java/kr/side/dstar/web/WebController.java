47e96a7f7c2f9fcc8acaded1aedd770dda89157e
daeun <demon0914@naver.com>
2020-04-12 04:39:28 +0900

    mustache 기본 설정 (추후 react로 프론트 바꾸자)

---
+++ src/main/java/kr/side/dstar/web/WebController.java
@@ -0,0 +1,12 @@
+package kr.side.dstar.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
