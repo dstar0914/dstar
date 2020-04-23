2835ac72ac1a1dd4d1a3e66d8d6c5e2d51685cd9
daeun <demon0914@naver.com>
2020-04-23 22:25:00 +0900

    애플리케이션 메인 메소드 변경

--- src/main/java/kr/side/dstar/DstarApplication.java
+++ src/main/java/kr/side/dstar/DstarApplication.java
@@ -2,6 +2,7 @@ package kr.side.dstar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,6 +13,8 @@ public class DstarApplication {
 			+ "/test-service/real-application.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(DstarApplication.class, args);
+		new SpringApplicationBuilder(DstarApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
