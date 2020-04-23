2a0e6a12e75697fd54fdb12b28908a736f15f98b
daeun <demon0914@naver.com>
2020-04-23 22:10:13 +0900

    profile 추가

--- src/main/java/kr/side/dstar/DstarApplication.java
+++ src/main/java/kr/side/dstar/DstarApplication.java
@@ -7,6 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class DstarApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/test-service/real-application.yml";
+
 	public static void main(String[] args) {
 		SpringApplication.run(DstarApplication.class, args);
 	}
