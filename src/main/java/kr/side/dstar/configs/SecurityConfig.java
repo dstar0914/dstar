516c8cf244420e7f29d53a1f60bd5fb6e480ba79
daeun <demon0914@naver.com>
2020-04-26 04:33:39 +0900

    SecurityConfig config 변경

--- src/main/java/kr/side/dstar/configs/SecurityConfig.java
+++ src/main/java/kr/side/dstar/configs/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,9 +39,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder);
     }
 
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().mvcMatchers("/docs/index.html");
+//        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().mvcMatchers("/docs/index.html");
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/docs/index.html").anonymous()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous();
     }
 }
